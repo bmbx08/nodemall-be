3eed6991ef2806a772dc1bc83db0f2716094efcf
bmbx08 <bmbx08@bu.ac.kr>
2025-05-04 23:03:42 +0900

    first commit

---
+++ model/Order.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const User = require('./User');
+const Product = require('./Product');
+const Schema = mongoose.Schema;
+const orderSchema = Schema({
+    userId:{type:mongoose.ObjectId,ref:User},
+    status:{type:String,default:"preparing"},
+    totalPrice:{type:Number, required:true,default:0},
+    shipTo:{type:Object,required:true},
+    contact:{type:Object,required:true},
+    orderNum:{type:String},
+    items:[{
+        productId:{type:mongoose.ObjectId,ref:Product,required:true},
+        qty:{type:Number,required:true},
+        size:{type:String,required:true, default:1},
+        price:{type:Number,required:true},
+    }]
+},{timestamps:true})
+
+orderSchema.methods.toJSON = function () {
+    const obj = this._doc;
+    delete obj.___v;
+    delete obj.updateAt;
+    delete obj.createAt;
+    return obj;
+}
+
+const Order = mongoose.model('Order', orderSchema);
+module.exports = Order
