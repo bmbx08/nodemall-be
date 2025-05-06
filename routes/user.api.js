688e2c42d6ff12203560732b0b2412054bd1a3c5
bmbx08 <bmbx08@bu.ac.kr>
2025-05-07 02:36:17 +0900

    added token login logic

--- routes/user.api.js
+++ routes/user.api.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const userController = require("../controllers/user.controller");
+const authController = require("../controllers/auth.controller");
 const router = express.Router();
 
 router.post("/",userController.createUser)
+router.get("/me",authController.authenticate,userController.getUser)//토큰이 valid한 토큰인지, 이후 token가지고 유저를 찾아서 return
 
 module.exports = router;
